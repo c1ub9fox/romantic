48fe9f93d9caf6e8c698bb30be138bdc5705291b
c1ub9fox <wufan980325@gmail.com>
2023-08-10 23:02:39 +0800

    update url

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,15 @@
 
 from django.urls import path,re_path
 from .views import CmdbView
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register('cmdb',CmdbView)
 
 urlpatterns = [
-    path('cmdb/', CmdbView.as_view({'get': 'list', 'post': 'create'})),
+    # path('cmdb/', CmdbView.as_view({'get': 'list', 'post': 'create'})),
     # re_path('cmdb/(\d+)',CmdbDetailView.as_view())
-    re_path('cmdb/(?P<pk>\d+)',CmdbView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}))
+    # re_path('cmdb/(?P<pk>\d+)',CmdbView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}))
 ]
+urlpatterns += router.urls
